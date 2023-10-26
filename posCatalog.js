0b5a1508f307e533fb8520d20580ff2b11251c04
BroncoWeb <149101377+BroncoWeb@users.noreply.github.com>
2023-10-26 10:52:37 -0700

    Update posCatalog.js

--- posCatalog.js
+++ posCatalog.js
@@ -1,6 +1,6 @@
 const wrapperEl = document.getElementById('pos-wrapper')
 const url = 'https://api.airtable.com/v0'
-const posToken = 'patLkaQGaci95XsiP.3856171d1bcdcd1c788c364c20605239f3cf114daa5052055c4b92addf0ce8bd'
+const posToken = ${{ secrets.API_TOKEN }}
 const posBaseId = 'appnP2hrGHs3uGTQ1'
 const posTableId = 'tblfb53l2JHHJODli'
 
