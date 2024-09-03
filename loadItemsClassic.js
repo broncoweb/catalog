cc820b92d2a3f4d4228e5737361733ed020160b8
bbrazis <32001385+bbrazis@users.noreply.github.com>
2024-09-03 12:11:29 -0700

    Update loadItemsClassic.js

--- loadItemsClassic.js
+++ loadItemsClassic.js
@@ -5,35 +5,45 @@ async function getItems(company) {
     return data
 }
 
-async function loadItems(company) {
+function buildItems(arr, template) {
+    const wrapper = document.getElementById('root')
+    for(const item of arr){
+        const { image_url, name, number, inventory, type } = item
+        if(inventory > 0){
+            const clone = template.content.cloneNode(true)
+            const img = clone.querySelector('.resource-image')
+            const title = clone.querySelector('.resource-title')
+            const cardType = clone.querySelectorAll('.resource-type')[0]
+            const cardInventory = clone.querySelectorAll('.resource-type')[1]
+            const cardNumber = clone.querySelector('.resource-number')
+            if(image_url){
+                clone.querySelector('.resource-image-text-wrapper').style.display = "none"
+                img.src = image_url
+            } else {
+                img.style.display = "none"
+            }
+            title.textContent = name
+            title.setAttribute('fs-cmsfilter-field','title')
+            cardType.textContent = type
+            cardType.setAttribute('fs-cmsfilter-field','type')
+            cardInventory.textContent = inventory
+            cardNumber.textContent = number
+            cardNumber.setAttribute('fs-cmsfilter-field','number')
+            wrapper.append(clone)
+        }
+    }
+}
+
+async function initialize(company) {
     if ("content" in document.createElement("template")) {
-        const wrapper = document.getElementById('root')
         const template = document.getElementById('pos-card') || null
         if(template) {
             const dataArr = await getItems(company)
-            
-            for(const item of dataArr){
-                const clone = template.content.cloneNode(true)
-                const { image_url, name, number, inventory, type } = item
-                const img = clone.querySelector('.resource-image')
-                const title = clone.querySelector('.resource-title')
-                const cardType = clone.querySelectorAll('.resource-type')[0]
-                const cardInventory = clone.querySelectorAll('.resource-type')[1]
-                const cardNumber = clone.querySelector('.resource-number')
-                if(image_url){
-                    clone.querySelector('.resource-image-text-wrapper').style.display = "none"
-                    img.src = image_url
-                } else {
-                    img.style.display = "none"
-                }
-                title.textContent = name
-                cardType.textContent = type
-                cardInventory.textContent = inventory
-                cardNumber.textContent = number
-                wrapper.append(clone)
+            if(dataArr){
+                buildItems(dataArr, template)
             }
         }
     }
 }
 
-loadItems("classic")
+initialize("classic")
