b2b0a3f666d117c8da16f33f05be4297d8c96fca
bbrazis <32001385+bbrazis@users.noreply.github.com>
2024-09-04 11:24:36 -0700

    Update catalogLoad.js

--- catalogLoad.js
+++ catalogLoad.js
@@ -1,20 +1,17 @@
 window.fsAttributes = window.fsAttributes || []
-window.fsAttributes.push([
-    'cmsload', 
-    async (listInstances) => {
-        console.log('cmsload Successfully loaded!')
+window.fsAttributes.push(['cmsload', async (listInstances) => {
+    console.log('cmsload Successfully loaded!')
 
-        const [listInstance] = listInstances //defines cms list being used
-        const [item] = listInstance.items //defines single item from reference list
-        const itemTemplate = item.element //defines template using single item
+    const [listInstance] = listInstances //defines cms list being used
+    const [item] = listInstance.items //defines single item from reference list
+    const itemTemplate = item.element //defines template using single item
+		
+    const cards = await fetchItems() //grabs data from external api
+    listInstance.clearItems() //removes placeholder items
 
-        const cards = await fetchItems() //grabs data from external api
-        listInstance.clearItems() //removes placeholder items
-        console.log(listInstance)
-        const newItems = cards.map(async (card) => await newItem(card, itemTemplate))
-        await listInstance.addItems(newItems)
-    }
-])
+    const newItems = cards.map((card) => newItem(card, itemTemplate))
+    await listInstance.addItems(newItems)
+}])
 
 const fetchItems = async () => {
     try {
@@ -27,14 +24,14 @@ const fetchItems = async () => {
     }
 }
 
-const newItem = async (item, template) => {
+const newItem = (item, template) => {
     const { image_url, name, number, inventory, type } = item
-    const clone = await template.cloneNode(true)
-    const img = await clone.querySelector('[data-element="image"]')
-    const title = await clone.querySelector('[data-element="title"]')
-    const cardType = await clone.querySelector('[data-element="type"]')
-    const cardInventory = await clone.querySelector('[data-element="inventory"]')
-    const cardNumber = await clone.querySelector('[data-element="number"]')
+    const clone = template.cloneNode(true)
+    const img = clone.querySelector('[data-element="image"]')
+    const title = clone.querySelector('[data-element="title"]')
+    const cardType = clone.querySelector('[data-element="type"]')
+    const cardInventory = clone.querySelector('[data-element="inventory"]')
+    const cardNumber = clone.querySelector('[data-element="number"]')
 
     if(image_url){
         if(img) {
@@ -44,6 +41,7 @@ const newItem = async (item, template) => {
     } else {
         if(img) img.classList.add('display-none')
     }
+    
     if(title) title.textContent = name
     if(cardType) cardType.textContent = type
     if(cardInventory) cardInventory.textContent = inventory
